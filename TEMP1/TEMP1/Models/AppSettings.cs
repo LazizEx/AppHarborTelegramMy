f36d709de595ed37e3fee76db7a524fe454ed9a8
LazizEx <laziz@mail.ru>
2017-08-10 20:42:21 +0300

    Site name "telegrambot-3.apphb.com"

--- TEMP1/TEMP1/Models/AppSettings.cs
+++ TEMP1/TEMP1/Models/AppSettings.cs
@@ -7,7 +7,8 @@ namespace TEMP1.Models
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://tempolx.azurewebsites.net:443/{0}";
+        public static string Url { get; set; } = "https://telegrambot-3.apphb.com:443/{0}";
+        //http://telegrambot-3.apphb.com/
         //public static string Name { get; set; } = "TempSSS_bot";
         public static string Key { get; set; } = "398231543:AAGAZ5M5NB_dI3aMFVVopys1dOniGAdWEx8";
     }
