0864f4c4cdfbf4ad9d4ba1d7b896796e5e199d42
LazizEx <laziz@mail.ru>
2017-08-11 08:56:56 +0300

    test

--- TEMP1/TEMP1/Controllers/MessageController.cs
+++ TEMP1/TEMP1/Controllers/MessageController.cs
@@ -16,12 +16,38 @@ namespace TEMP1.Controllers
         [Route("api/message/update")] // webhook uri api
         public async Task<IHttpActionResult> Update([FromBody]Update update)
         {
+            var client = await Bot.Get();
+            var message = update.Message;
+
             if (update.Message.Type == MessageType.TextMessage)
             {
-                var message = update.Message;
-                var client = await Bot.Get();
                 await client.SendTextMessageAsync(message.Chat.Id, "HI");
                 await client.SendTextMessageAsync(message.Chat.Id, message.Text);
+            }
+
+            switch (update.Type)
+            {
+                case UpdateType.UnkownUpdate:
+                    await client.SendTextMessageAsync(message.Chat.Id, update.Type.ToString());
+                    break;
+                case UpdateType.MessageUpdate:
+                    await client.SendTextMessageAsync(message.Chat.Id, update.Type.ToString());
+                    break;
+                case UpdateType.InlineQueryUpdate:
+                    await client.SendTextMessageAsync(message.Chat.Id, update.Type.ToString());
+                    break;
+                case UpdateType.ChosenInlineResultUpdate:
+                    await client.SendTextMessageAsync(message.Chat.Id, update.Type.ToString());
+                    break;
+                case UpdateType.CallbackQueryUpdate:
+                    await client.SendTextMessageAsync(message.Chat.Id, update.Type.ToString());
+                    break;
+                case UpdateType.EditedMessage:
+                    await client.SendTextMessageAsync(message.Chat.Id, update.Type.ToString());
+                    break;
+                default:
+                    await client.SendTextMessageAsync(message.Chat.Id, update.Type.ToString());
+                    break;
             }
             return Ok();
         }
